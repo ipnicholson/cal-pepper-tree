6c02851710e6d33eee7f198a480f1519b124409a
Ian Nicholson <ipnicholson@gmail.com>
2017-12-29 22:26:26 -0800

    move keys to secrets.yml

--- bot.rb
+++ bot.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/env ruby
 # ^ above is not a comment, but a "hashbang" DO NOT DELETE
 
+require 'yaml'
 require 'twitter'
 
+account = YAML.load_file('secrets.yml')
+
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "tU8llLczxP76UeOHddlrd9pU4"
-  config.consumer_secret     = "TQrwwKgP7OKfWzm1FHYPprVJAC21zrxUTYbnYlToUSUZWuB8uU"
-  config.access_token        = "935720336686854144-WhESir4Erih3OCoHIkl5kIDDWEamgpb"
-  config.access_token_secret = "7DFqLbl0L3yXnMda5o3RNs4XYUYaY0xSWSsl9flo15EDo"
+  config.consumer_key        = account['consumer_key']
+  config.consumer_secret     = account['consumer_secret']
+  config.access_token        = account['access_token']
+  config.access_token_secret = account['access_token_secret']
 end
 
 reply_message = "Native plants are great! But, did you know the 'California' Pepper Tree is actually an invasive weed from Peru? More on this mixup: https://en.wikipedia.org/wiki/Schinus_molle"

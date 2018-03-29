ad74369146b710889196b407cdce0b9abc1645b7
Ian Nicholson <ipnicholson@gmail.com>
2018-03-29 16:19:08 -0700

    encapsulate reply function

--- app.rb
+++ app.rb
@@ -5,8 +5,6 @@ require 'csv'
 require 'twitter'
 require 'pp'
 
-puts "Running!"
-
 # Load credentials
 account = YAML.load_file('secrets.yml')
 
@@ -17,14 +15,16 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = account['access_token_secret']
 end
 
-bot_account_id = client.user.id
-
 puts "\nBot Account: @#{client.user.screen_name}"
 
 # Search terms for tweets to reply to
 search_terms = "california pepper tree"
 
 puts "\nSearch Terms: \"#{search_terms}\""
+
+puts "\nRun? (Y/n)"
+answer = gets.chomp
+puts answer
 
 # Search options. See Twitter API docs
 search_options = {
@@ -34,32 +34,41 @@ search_options = {
 # Message to reply with
 reply_message = "Native plants are great! But, did you know the 'California' Pepper Tree is actually an invasive weed from Peru? More on this mixup: https://en.wikipedia.org/wiki/Schinus_molle"
 
-# Array of previously replied-to tweets
-replied_to = []
+def tweet_reply(client, search_terms, search_options, reply_message)
 
-CSV.foreach("replied-to.csv") do |row|
-  replied_to << row.first.to_i
-end
+  @client = client
+  @search_terms = search_terms
+  @search_options = search_options
+  @reply_message = reply_message
 
-# Reply to tweets matching search criteria
-client.search(search_terms, search_options).each do |tweet|
-  # Don't reply to self, and don't reply if self has already replied to that tweet
-  if (tweet.user.id != bot_account_id && !replied_to.include?(tweet.id))
-    begin
-      puts "\nMatching result: \n#{tweet.created_at} \n#{tweet.user.screen_name}: #{tweet.text}"
-      client.update("@#{tweet.user.screen_name} #{reply_message}", in_reply_to_status_id: tweet.id)
+  # Array of previously replied-to tweets
+  @replied_to = []
 
-    # Error handling
-    rescue Exception => error
-      puts "\nError:"
-      pp error.message
+  CSV.foreach("replied-to.csv") do |row|
+    @replied_to << row.first.to_i
+  end
 
-    # Log to CSV if no errors
-    else
-      CSV.open("replied-to.csv", "a") do |csv|
-        csv << [tweet.id, tweet.user.screen_name]
+  # Reply to tweets matching search criteria
+  client.search(@search_terms, @search_options).each do |tweet|
+    # Don't reply to self, and don't reply if self has already replied to that tweet
+    if (tweet.user.id != @client.user.id && !@replied_to.include?(tweet.id))
+      begin
+        puts "\nMatching result: \n#{tweet.created_at} \n#{tweet.user.screen_name}: #{tweet.text}"
+        client.update("@#{tweet.user.screen_name} #{@reply_message}", in_reply_to_status_id: tweet.id)
+
+      # Error handling
+      rescue Exception => error
+        puts "\nError:"
+        pp error.message
+
+      # Log to CSV if no errors
+      else
+        CSV.open("replied-to.csv", "a") do |csv|
+          csv << [tweet.id, tweet.user.screen_name]
+        end
       end
     end
   end
-
 end
+
+tweet_reply(client, search_terms, search_options, reply_message)
